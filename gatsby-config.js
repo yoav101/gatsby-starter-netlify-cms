7709e6f6063e360f12e09b6f91390272aa1d574b
yoav <yoavaharoni101@gmail.com>
2022-02-21 13:39:11 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,6 @@ module.exports = {
         purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    // "gatsby-plugin-netlify", // make sure to keep it last in the array
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
